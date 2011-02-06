2e10c068937657391eee698fe5e1266231f2d19b
Philip (flip) Kromer <flip@infochimps.org>
2011-02-06 06:17:59 -0600

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lunchlady::Application.config.secret_token = '06aa6377e79b7cd77b132dbf1dd09d5a07ec4a47fca1625a9d5d3e623c44da6eeb23f4c745d24c9c84e327351c46112adba0d04b51ac0dc58d737c61cee5d511'

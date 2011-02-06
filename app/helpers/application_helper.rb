15e9902d6a3a97f2202b37abb6ca0804ffa2416c
Philip (flip) Kromer <flip@infochimps.org>
2011-02-06 06:18:01 -0600

    Generated Helpers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,34 @@
-module ApplicationHelper
-end
+ module ApplicationHelper
+
+   # Help individual pages to set their HTML titles
+   def title(text)
+     @title = text
+   end
+
+   # This template will handle its own rows
+   def multirow
+     @multirow = true
+   end
+
+   # Help individual pages to set their HTML meta descriptions
+   def description(text)
+     content_for(:description){ text }
+   end
+
+   #
+   # Link to a resource by its titleized text
+   #
+   # link_to_rsrc @dataset  => <a href="/datasets/dataset-handle" dataset.title
+   # link_to_rsrc Dataset   => /datasets
+   #
+   def link_to_rsrc rsrc, options={}
+     return '' unless rsrc
+     case rsrc
+     when ActiveRecord::Base then dest = rsrc                        ; text = rsrc.titleize
+     when Class              then dest = url_for(rsrc.to_s.tableize) ; text = rsrc.to_s.titleize.pluralize
+     when Symbol             then dest = rsrc                        ; text = rsrc.to_s.titleize.pluralize
+     else                         dest = rsrc
+     end
+     link_to(text, dest, options)
+   end
+ end

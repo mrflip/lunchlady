2bc713010a8a633612c878923ec0ce9431efb5be
Philip (flip) Kromer <flip@infochimps.org>
2011-02-06 06:36:33 -0600

    pulled over restaurant views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,58 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+class Hash
+  # Create a hash from an array of keys and corresponding values.
+  def self.zip(keys, values, default=nil, &block)
+    hash = block_given? ? Hash.new(&block) : Hash.new(default)
+    keys.zip(values){|key,val| hash[key]=val }
+    hash
+  end
+end
+
+class Array
+  def random
+    self[Kernel.rand(size)]
+  end
+end
+
+RESTAURANT_FIELDS = [:tags, :name, :url, :menu_url, :review_url, :phone, :note]
+File.open(File.join(Rails.root,"/db/seeds/restaurants.tsv")) do |seed_file|
+  seed_file.each do |line|
+    values = line.strip.split("\t").map(&:strip)
+    fields = Hash.zip(RESTAURANT_FIELDS, values)
+    fields[:delivers] = (fields[:note].to_s =~ /^(no\W*)/i ? nil : true )
+    fields[:note] = fields[:note].to_s.gsub(/^(no|yes)\W*/i,"")
+    r = Restaurant.find_or_create_by_name fields[:name]
+    r.update_attributes fields
+  end
+end
+
+USER_FIELDS = [:id, :username, :email, :name, :local]
+File.open(File.join(Rails.root,"/db/seeds/users.tsv")) do |seed_file|
+  seed_file.each do |line|
+    values = line.strip.split("\t").map(&:strip)
+    fields = Hash.zip(USER_FIELDS, values)
+    u = User.find_or_create_by_id fields[:id]
+    u.update_attributes fields
+  end
+end
+
+all_restaurants = Restaurant.all.to_a
+(-2 .. 10).each do |offset|
+  order = Order.for_date(Date.today + offset)
+  if (rand(10) <= 5)
+    r = all_restaurants.random
+    order.restaurant = r
+    order.save!
+  end
+end
+
+
+all_users  = User.all.to_a
+Order.all.each do |order|
+  (3 + rand(3)).times do
+    user = all_users.random
+    meal = Meal.find_or_create_by_order_id_and_user_id(order.id, user.id)
+    meal.description = %w[dog cat horse pig elephant].random
+    meal.price       = 6.50 + rand(5.0)
+    meal.save!
+  end
+end

3cfe4254fc9216f54561d3f2d257dc492806c62e
Philip (flip) Kromer <flip@infochimps.org>
2011-02-06 06:18:00 -0600

    Generated boilerplate.

---
+++ config/compass.rb
@@ -0,0 +1,18 @@
+require 'html5-boilerplate'
+require 'ninesixty'
+
+project_type     = :rails
+project_path     = Compass::AppIntegration::Rails.root
+http_path        = "/"
+css_dir          = "public/stylesheets"
+sass_dir         = "app/stylesheets"
+javascripts_dir  = "public/javascripts"
+images_dir       = "public/images"
+environment      = Compass::AppIntegration::Rails.env
+preferred_syntax = :sass
+
+if Compass::AppIntegration::Rails.env == :development
+  output_style = :nested
+else
+  output_style = :compressed
+end

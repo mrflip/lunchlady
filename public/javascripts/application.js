83004d1ca19da87e0706310bcc2211e8802bdee8
Philip (flip) Kromer <flip@infochimps.org>
2011-02-08 01:09:33 -0600

    Javascript to recall past orders into the fill-in box

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,11 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function() {
+  $('#previous_orders').change(function() {
+    description_and_price = eval($(this).val());
+    $("#order_description").text(description_and_price[0]);
+    $("#order_price"       ).val(description_and_price[1]);
+    $("#inplace_edit_banner").text("Make sure to hit submit!")
+  });
+});

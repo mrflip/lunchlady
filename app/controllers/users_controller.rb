1628bd6db7f1c21c4428178d8950cbcfbf28a3ab
Philip (flip) Kromer <flip@infochimps.org>
2011-06-22 10:19:04 -0500

    users can sort

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,12 @@ class UsersController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @users = User.by_usage
+    case params[:order].to_s
+    when 'id'     then @users = User.by_id
+    when 'name'   then @users = User.alphabetically
+    when 'usage'  then @users = User.by_usage
+    else               @users = User.by_usage
+    end
   end
 
   def show

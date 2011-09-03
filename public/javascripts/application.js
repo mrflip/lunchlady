fc80f6921ccd3c92598c033a030b8c29e2b53fbd
Philip (flip) Kromer <flip@infochimps.org>
2011-09-02 23:08:19 -0400

    fixing js for reordering

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,8 +4,8 @@
 $(document).ready(function() {
   $('#previous_orders').change(function() {
     description_and_price = eval($(this).val());
-    $("#order_description").text(description_and_price[0]);
-    $("#order_price"       ).val(description_and_price[1]);
+    $("#order_description").text(description_and_price[1]);
+    $("#order_price"       ).val(description_and_price[2]);
     $("#inplace_edit_banner").text("Make sure to hit submit!")
   });
 
